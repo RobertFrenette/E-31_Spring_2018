5ac7afd866d97157163731784e4d0cd6ee205530
Robert Frenette <robert.frenette@comcast.net>
2018-03-26 14:34:46 -0400

    Added MVC demo.

---
+++ 15_Express_HBS_MVC/README.md
@@ -0,0 +1,13 @@
+# MVC with Express and HBS
+In this Section we'll look at implemetning the MVC pattern with Express and HBS.
+
+## Execute in Terminal
+```
+$ cd demo
+$ npm install
+  ...
+$
+```
+
+### Launch in Browser
++ [http://localhost:3000/mountains](http://localhost:3000/mountains)
---
+++ 15_Express_HBS_MVC/demo/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+ 
+var express    = require('express');
+var hbs        = require('hbs');
+var path       = require('path');
+var mongoose   = require('mongoose');
+var bodyparser = require('body-parser');
+var log        = require('log-util');
+
+var mountains  = require('./routes/mountains.js');
+ 
+// Connect to DB
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_USER_PWD}@cluster0-shard-00-00-rvadh.mongodb.net:27017,cluster0-shard-00-01-rvadh.mongodb.net:27017,cluster0-shard-00-02-rvadh.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
+mongoose.connect(uri)
+        .then(() => { log.info('DB connection successful!'); })
+        .catch((err) => { log.error(`DB connection failed: ${err}`); });
+ 
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials'); 
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/mountains', mountains);
+ 
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+app.use((req, res, next) => {
+    log.error(`404: ${req.url}`);
+    res.status(404).end('Error: 404 - Page Not Found.');
+});
+ 
+app.use((req, res, next) => {
+    log.error(`500: ${req.url}`);
+    res.status(500).end('Error!');
+});
+
+module.exports = app;
